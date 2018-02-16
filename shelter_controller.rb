bc0ad4a67348e036460f2666c6a2d42048419deb
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-16 16:04:16 +0000

    local host showing list of animals

---
+++ shelter_controller.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( './models/animals')
+
+get '/animals' do
+  @animal = Animal.all()
+  erb(:index)
+end
