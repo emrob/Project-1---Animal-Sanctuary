031e01abc991876c1715eec8e045d24466c15c20
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-17 16:34:03 +0000

    add new owner working

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -8,3 +8,13 @@ get '/owners' do
   @owner = Owner.all()
   erb(:"owners/index")
 end
+
+post '/owners' do
+  @owner = Owner.new(params)
+  @owner.save()
+  erb(:"owners/create")
+end
+
+get "/owners/new" do
+  erb(:"owners/new")
+end
