7add18a2a74e802188c2969bdd104e03cb8e963f
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-17 20:53:38 +0000

    adoption/success link showing but not yet working

--- controllers/adoption_controller.rb
+++ controllers/adoption_controller.rb
@@ -2,3 +2,9 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative( '../models/adoption')
+
+
+# get '/adoption' do
+#   @adoption = Adoption.all()
+#   erb(:"adoptions/index")
+# end
