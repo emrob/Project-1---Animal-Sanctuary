d6bd7c22ddfb797a7f683adac6186ed0d91e9bbc
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-19 10:12:26 +0000

    owner method working for animals - inner join

--- models/animals.rb
+++ models/animals.rb
@@ -12,7 +12,6 @@ class Animal
     @days_in_care = options['days_in_care'].to_i
     @adoption_status = options['adoption_status']
 
-
   end
 
 
@@ -21,29 +20,25 @@ class Animal
     values = [@name, @type, @age, @days_in_care, @adoption_status]
     animal = SqlRunner.run(sql, values)
     @id = animal.first()['id'].to_i
-    
-
   end
 
-  def delete()
-    sql = "DELETE FROM animals WHERE id = $1"
+  def owner()
+    sql = "SELECT * FROM owners INNER JOIN adoptions ON owners.id = adoptions.owner_id WHERE animal_id = $1"
     values = [@id]
-    SqlRunner.run(sql,values)
+    adoption = SqlRunner.run(sql,values)
+    return adoption.map {|adopt| Owner.new(adopt)}
 
   end
 
-  def self.delete_all()
-    sql= "DELETE FROM animals"
-    SqlRunner.run(sql)
 
-  end
+
+
 
   def self.all()
     sql = "SELECT * FROM animals"
     animals = SqlRunner.run(sql)
     result = animals.map { |animal| Animal.new(animal )}
-    return result
-
+    return
   end
 
   def self.find(id)
@@ -51,8 +46,19 @@ class Animal
     values = [id]
     animals = SqlRunner.run( sql, values)
     result = Animal.new(animals.first())
-    return result
+    return
+  end
 
+
+  def self.delete_all()
+    sql= "DELETE FROM animals"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM animals WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
   end
 
 
