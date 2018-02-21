12874c9639facd50b2ba91cd183676143300d106
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-21 11:41:55 +0000

    final touches

--- shelter_controller.rb
+++ shelter_controller.rb
@@ -5,7 +5,6 @@ require_relative('controllers/animal_controller')
 require_relative('controllers/owner_controller')
 require_relative('controllers/adoption_controller')
 
-
 get '/animal-shelter' do
   erb(:homepage)
 end
